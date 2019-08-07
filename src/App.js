ffd10085ac518ba26d91b1fb3d0cac1b9b577513
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2019-08-07 21:58:42 +0200

    VPN manager init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Table, Button} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Token</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>S10e</td>
+              <td>1234567890asdfg</td>
+              <td>
+                <Button color="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
